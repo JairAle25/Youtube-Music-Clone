c0ce457b4437cc71f26cd7608c44ca08c190ef85
Jair Cornejo <jaircornejo61@gmail.com>
2024-03-04 21:13:22 -0300

    comenzando la seccion de recomendaciones

--- src/components/cardMusic.jsx
+++ src/components/cardMusic.jsx
@@ -1,15 +1,21 @@
 import { useState } from "react";
 
-const CardMusic = () => {
+
+const CardMusic = ({imgUrl,nameSong,artistName,artistsLinkYoutube}) => {
+    const classNameRep='absolute top-[83%] left-[83%] transform -translate-x-1/2 -translate-y-1/2 bg-[#11090bd6] py-1 px-2 rounded-full text-2xl cursor-pointer transition-all duration-300 hover:bg-black hover:scale-110';
+
+    const [iconPlayer, setIconPlayer] = useState(true);
+
+
     return ( 
         <>
             <div className="flex flex-col max-w-[15%] relative">
-                <div className="relative">
-                    <img src="https://pbs.twimg.com/media/FQQolnsXEAIj4Un.jpg" alt="" className="rounded" />
-                    <i class="bi bi-play-fill absolute top-[83%] left-[83%] transform -translate-x-1/2 -translate-y-1/2 bg-[#11090bd6] py-1 px-2 rounded-full text-2xl cursor-pointer transition-all duration-300 hover:bg-black hover:scale-110"></i>
+                <div className="relative" >
+                    <img src={imgUrl} alt="" className="rounded cursor-pointer" onClick={()=> setIconPlayer(!iconPlayer)}/>
+                    {iconPlayer ? <i className={`bi bi-play-fill ${classNameRep}`} onClick={()=> setIconPlayer(!iconPlayer)}></i> : <i className={`bi bi-pause-fill ${classNameRep}`} onClick={()=> setIconPlayer(!iconPlayer)}></i>}
                 </div>
-                <h2 className="text-lg font-bold pt-2">BIEN O MAL</h2>
-                <p className="text-base"><i class="bi bi-explicit-fill text-[#b3b3b3]"></i> Canción - <a href="" className="hover:underline">Trueno</a></p>
+                <h2 className="text-base font-bold pt-2">{nameSong}</h2>
+                <p className="text-base pt-1 text-[#b0b0b0]"><i className="bi bi-explicit-fill text-[#b3b3b3]"></i> Canción - <a href={artistsLinkYoutube} className="hover:underline" target="_blank">{artistName}</a></p>
             </div>
         </>
      );
---
+++ src/constants/songs.js
@@ -0,0 +1,26 @@
+export const songs = [
+    {
+        urlImg : "https://pbs.twimg.com/media/FQQolnsXEAIj4Un.jpg",
+        nameSong :"BIEN O MAL",
+        artistName:"Trueno",
+        artistsLinkYoutube:"https://www.youtube.com/@TruenoOficial"
+    },
+    {
+        urlImg : "https://images.genius.com/9ab63389b4f9ca608664ca5250646066.1000x1000x1.jpg",
+        nameSong :"MILO J || BZRP Music Sessions #57",
+        artistName:"Milo j",
+        artistsLinkYoutube:"https://www.youtube.com/@miloj"
+    },
+    {
+        urlImg : "https://lh3.googleusercontent.com/HN9eGhFJrcSWmKO93q3Pmt_SG4EeuKknYrfC4W8oVq9JaQc9H0JelGQCXbrda-B0fYXJbe7wJACeXjF9=w544-h544-l90-rj",
+        nameSong :"Piel",
+        artistName:"Tiago PZK",
+        artistsLinkYoutube:"https://www.youtube.com/channel/UCMebXsaxWdvBzBNwus7TmxA"
+    },
+    {
+        urlImg : "https://rcbolivia.com/wp-content/uploads/2023/07/Luck-Ra-Morocha-Album-Artwork1.jpg",
+        nameSong :"La Morocha",
+        artistName:"Luck Ra",
+        artistsLinkYoutube:"https://www.youtube.com/@LuckRa"
+    },
+]
